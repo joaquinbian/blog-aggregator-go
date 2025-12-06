16ab921c1bb4e833e9b779dd6faeb7318cbfb870
joaquinbian <joacobian21@gmail.com>
2025-12-06 15:58:30 -0300

    addfeed command

--- sql/schema/0002_feeds.sql
+++ sql/schema/0002_feeds.sql
@@ -5,10 +5,10 @@ CREATE TABLE IF NOT EXISTS feeds (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name VARCHAR(50),
-    url VARCHAR(250),
+    name VARCHAR(50) NOT NULL,
+    url VARCHAR(250) NOT NULL,
     user_id UUID NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose StatementEnd
